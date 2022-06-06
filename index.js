b964ec44a0e3b326d844212940ec4f74457bdee9
ajithkumar0604 <ajithanish98@gmail.com>
2022-06-06 13:15:05 +0530

    added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 2000;
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(2000, () => {
+app.listen(port, () => {
   console.log("PORT 2000 is running");
 });
